a3be302e4f1f176ed1bbc214f023d3adcddcd4a3
brown-qs <neoprince1992@gmail.com>
2024-03-07 02:22:02 +0900

    more tokens

--- src/write.js
+++ src/write.js
@@ -1,3 +1,4 @@
 const buildList = require('./buildList');
 
-buildList().then(data => console.log(JSON.stringify(data, null, 2)));
+const data = buildList();
+console.log(JSON.stringify(data, null, 2));
